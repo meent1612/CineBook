1dff69d1f42175a6d6b1bd6a73b31c1f62ee7811
Rahnuma <mahjabin3619@gmail.com>
2026-03-30 01:53:33 +0600

    add seats table

--- database/migrations/07_seats.sql
+++ database/migrations/07_seats.sql
@@ -5,11 +5,10 @@ CREATE TABLE IF NOT EXISTS seats (
     hall_id     BIGINT UNSIGNED NOT NULL,
     row_label   VARCHAR(5)  NOT NULL,
     seat_number INT         NOT NULL,
-    seat_type   ENUM('regular', 'premium') NOT NULL DEFAULT 'regular',
+    seat_type   ENUM('standard', 'semi_recliner', 'premium', 'vip') NOT NULL DEFAULT 'standard',
     is_active   BOOLEAN NOT NULL DEFAULT TRUE,
     created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-
-    FOREIGN KEY (hall_id) REFERENCES halls(id) ON DELETE CASCADE,
-    UNIQUE KEY unique_seat (hall_id, row_label, seat_number)
+    UNIQUE KEY unique_seat (hall_id, row_label, seat_number),
+    FOREIGN KEY (hall_id) REFERENCES halls(id) ON DELETE CASCADE
 );
